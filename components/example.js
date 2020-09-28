a60bf32c678566fe92156ca5f32a0f44e2c7a65e
Eszter Szabó <eszticity@hotmail.com>
2020-09-28 21:16:45 +0200

    added example component for navigation

---
+++ components/example.js
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+import React, {Component} from 'react';
+
+export class Example extends Component
+{
+    render()
+    {
+        return (<View></View>);
+    }
+}
