684a72a960ee06121d7c7339f660aad00ea7a99c
Eszter Szabó <eszticity@hotmail.com>
2020-10-14 18:48:03 +0200

    fix

--- components/sections/header-component/components/header-menu-component/header-menu-component.js
+++ components/sections/header-component/components/header-menu-component/header-menu-component.js
@@ -14,31 +14,31 @@ export class HeaderMenuComponent extends Component {
             <View style={styles.openingMenuContainer}>
 
                 <OpeningMenuElement
-                    source={require('../../../../Sources/Openingmenu/home_100px.png')}
+                    source={require('../../../../../Sources/Openingmenu/home_100px.png')}
                     text="Home"
                     onPress={() => this.onPress('Home')}
                 />
 
                 <OpeningMenuElement
-                    source={require('../../../../Sources/Openingmenu/star_100px.png')}
+                    source={require('../../../../../Sources/Openingmenu/star_100px.png')}
                     text="Kedvencek"
                     onPress={() => this.onPress('Favourites')}
                 />
 
                 <OpeningMenuElement
-                    source={require('../../../../Sources/Openingmenu/shopping_cart_100px.png')}
+                    source={require('../../../../../Sources/Openingmenu/shopping_cart_100px.png')}
                     text="Kosár"
                     onPress={this.onBasketPress.bind(this)}
                 />
 
                 <OpeningMenuElement
-                    source={require('../../../../Sources/Openingmenu/chat_100px.png')}
+                    source={require('../../../../../Sources/Openingmenu/chat_100px.png')}
                     text="Üzenetek"
                     onPress={() => this.onPress('Chat')}
                 />
 
                 <OpeningMenuElement
-                    source={require('../../../../Sources/Openingmenu/gender_neutral_user_100px.png')}
+                    source={require('../../../../../Sources/Openingmenu/gender_neutral_user_100px.png')}
                     text="Profil"
                     onPress={() => this.onPress('Profile')}
                 />
