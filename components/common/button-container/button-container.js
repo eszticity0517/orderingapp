8af3405bf6b53ebbaaac0e082e0e11ede67978c0
Eszter Szabó <eszticity@hotmail.com>
2020-10-16 00:25:46 +0200

    button-container.scss added

--- components/common/button-container/button-container.js
+++ components/common/button-container/button-container.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
-import {StyleSheet} from 'react-native';
+import styles from './button-container.scss';
 
 export class ButtonContainer extends Component
 {
@@ -17,20 +17,3 @@ export class ButtonContainer extends Component
         return (container);
     }
 }
-
-export const styles = StyleSheet.create({
-    buttonContainer: {
-        position: 'absolute',
-        backgroundColor: "white",
-        height: 50,
-        flexDirection: "row",
-        bottom: 0,
-        flex: 1,
-        alignSelf: 'stretch',
-        right: 0,
-        left: 0,
-        zIndex: 1,
-        paddingLeft: 20,
-        paddingRight: 20
-    }
-});
