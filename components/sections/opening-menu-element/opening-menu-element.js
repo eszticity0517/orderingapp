76d457e1be15263f11ae3e8ce6f56dc75b791ea0
Eszter Szabó <eszticity@hotmail.com>
2020-10-17 19:43:39 +0200

    opening-menu-element.scss added

--- components/sections/opening-menu-element/opening-menu-element.js
+++ components/sections/opening-menu-element/opening-menu-element.js
@@ -1,20 +1,21 @@
 import React, {Component} from 'react';
 import {Image, Text, TouchableOpacity, View} from 'react-native';
 import mainStyles from '../../main-styles.scss';
+import styles from './opening-menu-element.scss';
 
 export class OpeningMenuElement extends Component
 {
     render()
     {
         return (
-            <View flexDirection="row" style={{ height: 40 }}>
-                <TouchableOpacity style={{ flex: 0.5, justifyContent: "center", height: 40 }} onPress={this.props.onPress.bind(this)}>
+            <View flexDirection="row" style={styles.mainContainer}>
+                <TouchableOpacity style={styles.menuElementIcon} onPress={this.props.onPress.bind(this)}>
                     <Image
                         style={mainStyles.image}
                         source={this.props.source}
                     />
                 </TouchableOpacity>
-                <TouchableOpacity style={{ height: 40, flex: 1, justifyContent: "center" }} onPress={this.props.onPress.bind(this)}>
+                <TouchableOpacity style={styles.menuElementName} onPress={this.props.onPress.bind(this)}>
                     <Text>{this.props.text}</Text>
                 </TouchableOpacity>
             </View>
