b11edb6df983e8a3fbec436edef0ddbe3867beb8
Eszter Szabó <eszticity@hotmail.com>
2020-10-17 19:05:13 +0200

    total-component styling refactor

--- components/sections/total-component/total-component.js
+++ components/sections/total-component/total-component.js
@@ -1,27 +1,44 @@
 import React, {Component} from 'react';
 import {Text, View } from 'react-native';
 import '../../../global';
-import { styles } from './total-component.scss';
+import styles from './total-component.scss';
 
 export class TotalComponent extends Component
 {
     render()
     {
-        let background = this.props.light ? 'white' : '#77D353';
-        let color = this.props.light ? 'black' : 'white';
-
         return (
-            <View style={[styles.totalContainer, {
-                width: '100%',
-                backgroundColor: background,
-            }]}>
-                <View style={{ flex: 1, justifyContent: 'center' }}>
-                    <Text style={{ color: color }}>{this.props.text.toUpperCase()}</Text>
+            <View style={[styles.totalContainer, this._renderContainerStyle()]}>
+                <View style={styles.innerContainer}>
+                    <Text style={{ color: this._renderColor() }}>{this.props.text.toUpperCase()}</Text>
                 </View>
-                <View style={{ flex: 1, justifyContent: 'center' }}>
-                    <Text style={{ color: color, alignSelf: 'flex-end' }}>{this.props.sum} Ft.-</Text>
+                <View style={styles.innerContainer}>
+                    <Text style={this._renderPriceStyle()}>{this.props.sum} Ft.-</Text>
                 </View>
             </View>
         );
+    }
+
+    _renderPriceStyle()
+    {
+        return ({ color: this._renderColor(), alignSelf: 'flex-end' });
+    }
+
+    _renderColor()
+    {
+        return (this.props.light ? 'black' : 'white');
+    }
+
+    _renderContainerStyle()
+    {
+        return ({
+            width: '100%',
+            backgroundColor: this._renderBackgroundColor(),
+        });
+    }
+
+    _renderBackgroundColor()
+    {
+        return (this.props.light ? 'white' : '#77D353');
     }
 }
